f7de61a59d3f846d130bc7c90236c8cd4403aa84
Funk <funkfuze@gmail.com>
2024-09-01 14:28:38 +0700

    Kuy4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src', // หรือที่ที่คุณวาง index.html ไว้
   plugins: [react()],
   base: '/', // Adjust this if your app is served from a subdirectory
   build: {
