be70292b7f69524623f30a66d2bd685c8b7902a0
Funk <funkfuze@gmail.com>
2024-09-02 02:41:18 +0700

    Commit

--- src/components/Pages/Quiz.jsx
+++ src/components/Pages/Quiz.jsx
@@ -1,108 +1,101 @@
-import {Layout,Button} from "antd"
-import { ArrowRightOutlined } from "@ant-design/icons"
+import { Layout, Button } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 import Navbar from "../Nav/navbar";
 import Footers from "../Content/Footer/Footer";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { data } from "autoprefixer";
+import quizData from "./data/quizData.json"
 
 const Quiz = () => {
-  const [question,setQuestion] = useState("")
-  const [Answer,setAnswer] = useState([])
-  const [index,setIndex] = useState(0)
-  const [CorrectAnswer,setIscorrectAnswer] = useState(false)
-  const [score,setScore] = useState(0)
-  const addindex = () => { 
-    setIndex((prevIndex) => prevIndex+1)
-  }
-  const PrevQuiz = () => { 
-    setIndex((prevIndex) => prevIndex-1)
-  }
-  if (index < 0) { 
-    setIndex(0)
-  }
-  else if(index > 9){ 
-    setIndex(9)
-  }
-  
-  const getApi = async() =>{ 
-  await axios.get("https://funkapu.github.io/RestAPIEnglishFixers/exam.json")
-  .then((data) => {
-    if(data.data[index]){ 
-      console.log(data.data)
-      setQuestion(data.data[index].questionText)
-      setAnswer(data.data[index].answerOption.map((option) => option.answerText ))
-      setIscorrectAnswer(data.data[index].answerOption.map((Correct) => Correct.isCorrect))
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const fetchQuizData = () => {
+    if (quizData[index]) {
+      setQuestion(quizData[index].questionText);
+      setAnswer(quizData[index].answerOption.map((option) => option.answerText));
+      setCorrectAnswers(quizData[index].answerOption.map((option) => option.isCorrect));
     }
-    else{ 
-      console.log("No more quiz")
+  };
+
+  useEffect(() => {
+    fetchQuizData();
+  }, [index]);
+
+  const checkAnswer = (selectedIndex) => {
+    if (correctAnswers[selectedIndex]) {
+      setScore((prevScore) => prevScore + 1);
     }
-  })
-  .catch((err) => console.log(err))
-}
-const checkAnswer = (selectIndexAnswer) => { 
-  if(CorrectAnswer[selectIndexAnswer]){ 
-    setScore(score+1)
-    setIscorrectAnswer(true)
-  }else{ 
-    setIscorrectAnswer(false)
-  }
-}
-useEffect(() => { 
-  getApi()
-},[index])
-  return( 
-  <div>
-    <Layout>
-     <Navbar/>
-          <div className="flex justify-center items-center">
-            {index >= 9 ? ( 
-              <div>{score >= 6 ? ( 
-                <div className="my-[-30rem] space-y-4">
-                  <h1 className="flex justify-center text-3xl font-bold">คุณได้คะแนน : {score}</h1>
-                  <p className="flex justify-center font-semibold">คุณสามารถเรียนอะไรก็ได้</p>
-                  <Link to="/course" className="justify-center flex"><Button type="primary">ดูคอร์ส</Button></Link>
-                </div>
-              ):( 
-                <div className="my-[-30rem]">
-                  <h1 className="flex justify-center text-3xl font-bold">คุณได้คะแนน : {score}</h1>
-                  <p className="text-sm semibold flex justify-center font-semibold">ทางเราแนะนำให้คุณเรียน</p>
-                  <div className="flex justify-center list-none space-x-5 my-10">
-                  <li><Link to="/course/Grammar"><Button type="primary">Grammar</Button></Link></li>
-                  <li><Link to="/course/Vocab"><Button type="primary">Vocab</Button></Link></li>
-                  <li><Link to="/course/Conversation"><Button type="primary">Conversation</Button></Link></li>
-                  </div>
+  };
+
+  const handleNext = () => {
+    if (index < quizData.length - 1) {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (index > 0) {
+      setIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  return (
+    <div>
+      <Layout>
+        <Navbar />
+        <div className="flex justify-center items-center">
+          {index >= quizData.length - 1 ? (
+            <div className="">
+              <div className="space-y-4 my-[-35rem]">
+              <h1 className="flex justify-center text-3xl font-bold">คุณได้คะแนน : {score}</h1>
+              <p className="flex justify-center font-semibold">
+                {score >= 6 ? "คุณสามารถเรียนอะไรก็ได้" : "ทางเราแนะนำให้คุณเรียน"}
+              </p>
+              {score >= 6 ? (
+                <Link to="/course" className="justify-center flex">
+                  <Button type="primary">ดูคอร์ส</Button>
+                </Link>
+              ) : (
+                <div className="flex justify-center list-none space-x-5 my-10">
+                  <Link to="/course/Grammar"><Button type="primary">Grammar</Button></Link>
+                  <Link to="/course/Vocab"><Button type="primary">Vocab</Button></Link>
+                  <Link to="/course/Conversation"><Button type="primary">Conversation</Button></Link>
                 </div>
               )}
-              </div>
-            ):( 
-              <div className="text-2xl">
-          <div className="my-[-45rem]">
-            <p>{question}</p>
-            <ul>
-              {Answer.map((answer,i) => (
-                <li className="my-3 text-xl" key={i}><Button className="text-xl"
-                onClick={() =>{
-                  checkAnswer(i)
-                  addindex()
-                }}>{answer}</Button></li>
-              ))}
-            </ul>
-            <div className="QuizBtn space-x-5">
-              <Button onClick={PrevQuiz} type="primary" className="text-xl">PrevQuiz</Button>
-              <Button onClick={addindex} type="primary" className="text-xl">NextQuestion <ArrowRightOutlined /></Button>
             </div>
-          </div>
-          </div>
-            )}
-            
-          </div>
-          <div>
-        <Footers/>
+            </div>
+          ) : (
+            <div className="text-2xl">
+              <div className="my-[-45rem]">
+                <p>{question}</p>
+                <ul>
+                  {answer.map((ans, i) => (
+                    <li className="my-3 text-xl" key={i}>
+                      <Button className="text-xl" onClick={() => {
+                        checkAnswer(i);
+                        handleNext();
+                      }}>
+                        {ans}
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+                <div className="QuizBtn space-x-5">
+                  <Button onClick={handlePrev} type="primary" className="text-xl">PrevQuiz</Button>
+                  <Button onClick={handleNext} type="primary" className="text-xl">NextQuestion <ArrowRightOutlined /></Button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        </Layout>
-  </div>
-  )
+        <Footers />
+      </Layout>
+    </div>
+  );
 };
+
 export default Quiz;
